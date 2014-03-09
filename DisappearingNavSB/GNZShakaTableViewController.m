3906b4d0d08d3e1ee7937914931a2dfa44cbdbcb
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-03-09 19:55:16 -0400

    plugged in containers and subviews

--- DisappearingNavSB/GNZShakaTableViewController.m
+++ DisappearingNavSB/GNZShakaTableViewController.m
@@ -10,10 +10,14 @@
 
 @interface GNZShakaTableViewController () <UIScrollViewDelegate,UITableViewDataSource, UITableViewDelegate>
 @property (strong, nonatomic) IBOutlet UITableView *tableView;
-@property (nonatomic) CGFloat lastOffset;
 @property (strong, nonatomic) UIView *rightBarButtonContainer;
-@property (strong, nonatomic) UILabel *label;
 @property (strong, nonatomic) UIButton *rightBarButton;
+@property (strong, nonatomic) UIView *barTitleContainer;
+@property (strong, nonatomic) UILabel *barTitleLabel;
+@property (strong, nonatomic) UIView *leftBarButtonContainer;
+@property (strong, nonatomic) UIButton *leftBarButton;
+@property (strong, nonatomic) NSArray *buttonList;
+@property (strong, nonatomic) NSArray *containerList;
 
 @end
 
@@ -24,22 +28,45 @@
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
     
-    self.navigationItem.title = @"NeatView";
-    
+//Right Bar Button
     self.rightBarButtonContainer = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 32, 32)];
-    self.rightBarButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    self.rightBarButton.frame = CGRectMake(0, 0, 32, 32);
-    [self.rightBarButton setTitle:@"\U0001F4CC" forState:UIControlStateNormal];
-    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.frame = CGRectMake(0, 0, 32, 32);
+    [button setTitle:@"\U0001F4CC" forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    self.rightBarButton = button;
     [self.rightBarButtonContainer addSubview:self.rightBarButton];
     
-    UIBarButtonItem *custom = [[UIBarButtonItem alloc] initWithCustomView:self.rightBarButtonContainer];
-    self.navigationItem.rightBarButtonItem = custom;
+    UIBarButtonItem *rightButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.rightBarButtonContainer];
+    self.navigationItem.rightBarButtonItem = rightButtonItem;
     
-    self.label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 32, 32)];
-    [self.label setCenter:CGPointMake(self.rightBarButtonContainer.frame.size.width / 2, self.rightBarButtonContainer.frame.size.height / 2)];
+//Title Label
+    self.barTitleContainer = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 200, 32)];
+    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 200, 32)];
+    [titleLabel setCenter:CGPointMake(self.rightBarButtonContainer.frame.size.width / 2, self.rightBarButtonContainer.frame.size.height / 2)];
+    [titleLabel setFont:[UIFont fontWithName:@"Helvetica-Bold" size:18]];
+    [titleLabel setBackgroundColor:[UIColor clearColor]];
+    [titleLabel setTextColor:[UIColor blackColor]];
+    [titleLabel setText:@"TitleTitleTitleTitleTitleTitleTitleTitleTitle"];
+    [titleLabel setTextAlignment:NSTextAlignmentCenter];
+    self.barTitleLabel = titleLabel;
+    self.navigationItem.titleView = self.barTitleLabel;
     
-
+//Left Bar Button
+    self.leftBarButtonContainer = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 32, 32)];
+    button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.frame = CGRectMake(0, 0, 32, 32);
+    [button setTitle:@"\u2705" forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    self.leftBarButton = button;
+    [self.leftBarButtonContainer addSubview:self.leftBarButton];
+    
+    UIBarButtonItem *leftButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.leftBarButtonContainer];
+    self.navigationItem.leftBarButtonItem = leftButtonItem;
+    
+//Collect Views for bulk adjustments
+    self.buttonList = @[self.rightBarButton, self.barTitleLabel, self.leftBarButton];
+    self.containerList = @[self.rightBarButtonContainer, self.barTitleContainer, self.leftBarButtonContainer];
 }
 
 #pragma mark - Table view data source
@@ -64,16 +91,6 @@
 # define kStatusBarHeight 20
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView
 {
-    // this should not be here lulz
-//    AUAuraTitleCell *titleCell = [self.profileTable dequeueReusableCellWithIdentifier:@"auraHeaderCell"];
-//    
-//    CGRect collectionViewFrame = self.profileTable.frame;
-//    CGRect subheaderFrame = titleCell.frame;
-//    CGRect headerFrame = self.headerBar.frame;
-//    CGRect screenFrame = self.view.frame;
-//    
-//    CGFloat headerHeight = headerFrame.size.height;
-//    CGFloat subHeaderHeight = subheaderFrame.size.height;
     
     CGFloat offset = scrollView.contentOffset.y;
     CGRect navBarFrame = self.navigationController.navigationBar.frame;
@@ -88,61 +105,82 @@
         CGRect tempRect = CGRectMake(navBarFrame.origin.x, kStatusBarHeight, navBarFrame.size.width, navBarFrame.size.height);
         self.navigationController.navigationBar.frame = tempRect;
 
-        self.rightBarButton.alpha = 1;
+        for (UIView *view in self.buttonList) {
+            view.alpha = 1;
+        }
+//        self.rightBarButton.alpha = 1;
         
-        [self.navigationController.navigationBar setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:
-                                                    [[UIColor blackColor] colorWithAlphaComponent:1],
-                                                    NSForegroundColorAttributeName,
-                                                    [UIFont fontWithName:@"Helvetica-Bold" size:16.0],
-                                                    NSFontAttributeName,
-                                                    nil]];
+//        [self.navigationController.navigationBar setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:
+//                                                    [[UIColor blackColor] colorWithAlphaComponent:1],
+//                                                    NSForegroundColorAttributeName,
+//                                                    [UIFont fontWithName:@"Helvetica-Bold" size:16.0],
+//                                                    NSFontAttributeName,
+//                                                    nil]];
+        for (UIView *view in self.buttonList) {
+            view.transform = CGAffineTransformMakeScale(1, 1);
+        }
         
-//
-//        self.profileTable.frame = CGRectMake(collectionViewFrame.origin.x, paddingTop, collectionViewFrame.size.width, screenFrame.size.height-paddingTop);
-        
-        self.rightBarButton.transform = CGAffineTransformMakeScale(1, 1);
+//        self.rightBarButton.transform = CGAffineTransformMakeScale(1, 1);
 
-        NSLog(@"Original Label Container Frame Y: %f", self.rightBarButtonContainer.frame.origin.y);
-        CGRect buttonContainerFrame = self.rightBarButton.frame;
-        buttonContainerFrame.origin.y = 0;
-        self.rightBarButton.frame = buttonContainerFrame;
+        for (UIView *view in self.containerList) {
+            CGRect buttonContainerFrame = view.frame;
+            buttonContainerFrame.origin.y = 0;
+            view.frame = buttonContainerFrame;
+        }
+ 
+//        CGRect buttonContainerFrame = self.rightBarButton.frame;
+//        buttonContainerFrame.origin.y = 0;
+//        self.rightBarButton.frame = buttonContainerFrame;
         
     } else if (offset/2>-64 && offset<=-20) {
         CGRect tempRect = CGRectMake(navBarFrame.origin.x, -navBarFrame.size.height-offset, navBarFrame.size.width, navBarFrame.size.height);
         self.navigationController.navigationBar.frame = tempRect;
         
-        [self.navigationController.navigationBar setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:
-                                                                         [[UIColor blackColor] colorWithAlphaComponent:headerRemains/navBarFrame.size.height],
-                                                                         NSForegroundColorAttributeName,
-                                                                         [UIFont fontWithName:@"Helvetica-Bold" size:16.0],
-                                                                         NSFontAttributeName,
-                                                                         nil]];
-//
-//        self.profileTable.frame = CGRectMake(collectionViewFrame.origin.x, paddingTop-adjustedOffset/2, collectionViewFrame.size.width, screenFrame.size.height-paddingTop+adjustedOffset/2);
-//        
-        self.rightBarButton.transform = CGAffineTransformMakeScale(1-(offset+64)/2/44, 1-(offset+64)/2/44);
-        
-        self.rightBarButton.alpha = headerRemains/navBarFrame.size.height;
-//
-        CGRect buttonContainerFrame = self.label.frame;
-        buttonContainerFrame.origin.y = (offset+64)/2;
+//        [self.navigationController.navigationBar setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:
+//                                                                         [[UIColor blackColor] colorWithAlphaComponent:headerRemains/navBarFrame.size.height],
+//                                                                         NSForegroundColorAttributeName,
+//                                                                         [UIFont fontWithName:@"Helvetica-Bold" size:16.0],
+//                                                                         NSFontAttributeName,
+//                                                                         nil]];
+        for (UIView *view in self.buttonList) {
+            view.alpha = headerRemains/navBarFrame.size.height;;
+        }
 
-        self.rightBarButton.frame = buttonContainerFrame;
+        for (UIView *view in self.buttonList) {
+            view.transform = CGAffineTransformMakeScale(1-(offset+64)/2/44, 1-(offset+64)/2/44);
+        }
+        
+//        self.rightBarButton.transform = CGAffineTransformMakeScale(1-(offset+64)/2/44, 1-(offset+64)/2/44);
+
+//        self.rightBarButton.alpha = headerRemains/navBarFrame.size.height;
+
+        for (UIView *view in self.containerList) {
+            CGRect buttonContainerFrame = view.frame;
+            buttonContainerFrame.origin.y = (offset+64)/2;
+            view.frame = buttonContainerFrame;
+        }
+        
+//        CGRect buttonContainerFrame = self.rightBarButton.frame;
+//        buttonContainerFrame.origin.y = (offset+64)/2;
+//        self.rightBarButton.frame = buttonContainerFrame;
         
     } else { // header completely gone
         CGRect tempRect = CGRectMake(navBarFrame.origin.x, kStatusBarHeight-navBarFrame.size.height, navBarFrame.size.width, navBarFrame.size.height);
         self.navigationController.navigationBar.frame = tempRect;
 
-        [self.navigationController.navigationBar setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:
-                                                                         [[UIColor blackColor] colorWithAlphaComponent:0],
-                                                                         NSForegroundColorAttributeName,
-                                                                         [UIFont fontWithName:@"Helvetica-Bold" size:16.0],
-                                                                         NSFontAttributeName,
-                                                                         nil]];
+//        [self.navigationController.navigationBar setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:
+//                                                                         [[UIColor blackColor] colorWithAlphaComponent:0],
+//                                                                         NSForegroundColorAttributeName,
+//                                                                         [UIFont fontWithName:@"Helvetica-Bold" size:16.0],
+//                                                                         NSFontAttributeName,
+//                                                                         nil]];
 //
 //        self.profileTable.frame = CGRectMake(collectionViewFrame.origin.x, paddingTop-headerHeight, collectionViewFrame.size.width, screenFrame.size.height-paddingTop+headerHeight);
+        for (UIView *view in self.buttonList) {
+            view.transform = CGAffineTransformMakeScale(0, 0);
+        }
         
-        self.rightBarButton.transform = CGAffineTransformMakeScale(0, 0);
+//        self.rightBarButton.transform = CGAffineTransformMakeScale(0, 0);
         
 
 //        self.buttView.frame = CGRectMake(0, 0, self.buttView.frame.size.width, self.buttView.frame.size.height);
